49eebc3139ac4a8e281e23d847eb576060cb93f7
slee2540 <slee2540@gmail.com>
2019-04-03 15:30:10 +0900

    세번째 커밋

--- src/api/auth/auth.controller.js
+++ src/api/auth/auth.controller.js
@@ -46,6 +46,16 @@ exports.localRegister = async (ctx) => {
         ctx.throw(500, e);
     }
 
+    let token = null;
+    try {
+        token = await account.generateToken();        
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+
+    
+    ctx.cookies.set('access_token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 7 });
+    // console.log(ctx.cookies)
     ctx.body = account.profile; // 프로필 정보로 응답합니다.
 };
 
@@ -80,7 +90,15 @@ exports.localLogin = async (ctx) => {
       return;
   }
 
-  ctx.body = account.profile;
+  let token = null;
+  try {
+      token = await account.generateToken();      
+  } catch (e) {
+      ctx.throw(500, e);
+  }
+
+  ctx.cookies.set('access_token', token, { httpOnly: true, maxAge: 1000 * 60 * 60 * 24 * 7 });
+  ctx.body = account.profile; // 프로필 정보로 응답합니다.
 };
 
 // 이메일 / 아이디 존재유무 확인
@@ -101,6 +119,10 @@ exports.exists = async (ctx) => {
 };
 
 // 로그아웃
-exports.logout = async (ctx) => {
-    ctx.body = 'logout';
+exports.logout = (ctx) => {
+  ctx.cookies.set('access_token', null, {
+      maxAge: 0, 
+      httpOnly: true
+  });
+  ctx.status = 204;
 };
