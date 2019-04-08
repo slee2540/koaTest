a4207321863fb5cd1b0c2157248f1b9f56d261bf
slee2540 <slee2540@gmail.com>
2019-04-08 20:12:36 +0900

    post 작업중

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,9 @@ const Router = require('koa-router');
 const api = new Router();
 
 const auth = require('./auth');
+const posts = require('./posts');
 api.use('/auth', auth.routes());
-
+api.use('/posts', posts.routes());
 // const books = require('./books');
 // api.use('/books', books.routes());
 
