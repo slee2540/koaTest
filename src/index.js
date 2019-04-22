3a4926e0e78568e6065fcfd03acb19f5a3e4bc07
slee2540 <slee2540@gmail.com>
2019-04-22 14:50:52 +0900

    websocket radis 연결

--- src/index.js
+++ src/index.js
@@ -1,30 +1,37 @@
-require('dotenv').config(); // .env 파일에서 환경변수 불러오기
+require("dotenv").config(); // .env 파일에서 환경변수 불러오기
 
-const Koa = require('koa');
-const Router = require('koa-router');
+const Koa = require("koa");
+const Router = require("koa-router");
+const websockify = require("koa-websocket");
 
-const app = new Koa();
+const app = websockify(new Koa());
 const router = new Router();
-const api = require('./api');
-const mongoose = require('mongoose');
-const bodyParser = require('koa-bodyparser');
-const { jwtMiddleware } = require('lib/token');
+const api = require("./api");
+const ws = require("./ws");
 
-mongoose.set('useFindAndModify', false);
+const mongoose = require("mongoose");
+const bodyParser = require("koa-bodyparser");
+const { jwtMiddleware } = require("lib/token");
+
+mongoose.set("useFindAndModify", false);
 mongoose.Promise = global.Promise; // Node 의 네이티브 Promise 사용
-mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true }).then(
-    (response) => {
-        console.log('Successfully connected to mongodb');
-    }
-).catch(e => {
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(response => {
+    console.log("Successfully connected to mongodb");
+  })
+  .catch(e => {
     console.error(e);
-});
+  });
 
 app.use(bodyParser()); // 바디파서 적용, 라우터 적용코드보다 상단에 있어야합니다.
 app.use(jwtMiddleware);
-router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
+router.use("/api", api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
 app.use(router.routes()).use(router.allowedMethods());
+app.ws.use(ws.routes()).use(ws.allowedMethods());
 
 const port = process.env.PORT || 4000; // PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
 
-app.listen(port, () => {console.log('heurm server is listening to port'+ port);});
+app.listen(port, () => {
+  console.log("heurm server is listening to port" + port);
+});
---
+++ src/ws/index.js
@@ -0,0 +1,30 @@
+const Router = require("koa-router");
+
+const redis = require("redis");
+
+// 두개의 redis 클라이언트 생성
+const subscriber = redis.createClient();
+
+subscriber.subscribe("posts"); // posts 채널 구독
+
+const ws = new Router();
+
+let counter = 0;
+
+ws.get("/ws", (ctx, next) => {
+  // 구독자가 message 받을 때 마다 해당 소켓에 데이터 전달
+  // 연결이 끊겼을 때 취소 할 수 있도록 따로 구분해줍니다
+  const listener = (channel, message) => {
+    // 메시지를 그대로 전달해줍니다
+    ctx.websocket.send(message);
+  };
+
+  subscriber.on("message", listener);
+
+  // 유저가 나갔을 때
+  ctx.websocket.on("close", () => {
+    subscriber.removeListener("message", listener);
+  });
+});
+
+module.exports = ws;
