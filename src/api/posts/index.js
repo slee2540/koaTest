962868f751db96e5980c21d551340b43cbfbfdf7
slee2540 <slee2540@gmail.com>
2019-04-22 19:36:48 +0900

    좋아요 기능 API

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -1,8 +1,11 @@
 const Router = require("koa-router");
 const posts = new Router();
 const postsCtrl = require("./posts.controller");
+const likesCtrl = require("./likes.controller");
 
 posts.post("/", postsCtrl.write);
 posts.get("/", postsCtrl.list);
+posts.post("/:postId/likes", likesCtrl.like);
+posts.delete("/:postId/likes", likesCtrl.unlike);
 
 module.exports = posts;
--- src/models/post.js
+++ src/models/post.js
@@ -45,4 +45,61 @@ Post.statics.list = function({ cursor, username, self }) {
     .exec();
 };
 
+Post.statics.like = function({ _id, username }) {
+  return this.findByIdAndUpdate(
+    _id,
+    {
+      $inc: { likesCount: 1 }, // likesCount 를 1 더하고
+      $push: { likes: username }
+    },
+    {
+      new: true, // 이걸 해야 업데이트 된 데이터를 반환함
+      select: "likesCount"
+    }
+  ).exec();
+};
+
+Post.statics.unlike = function({ _id, username }) {
+  return this.findByIdAndUpdate(
+    _id,
+    {
+      $inc: { likesCount: -1 },
+      $pull: { likes: username }
+    },
+    {
+      new: true,
+      select: "likesCount"
+    }
+  );
+};
+
+Post.statics.list = function({ cursor, username, self }) {
+  // cursor, username 값의 존재 유무에 따라 쿼리가 유동적으로 설정됩니다.
+  const query = Object.assign(
+    {},
+    cursor ? { _id: { $lt: cursor } } : {},
+    username ? { username } : {}
+  );
+
+  // API 를 호출한 username (self) 값이 존재하면 likes 에 해당 username 이 있는지 체크
+  const projection = self
+    ? {
+        count: 1,
+        username: 1,
+        content: 1,
+        comments: 1,
+        likes: {
+          $elemMatch: { $eq: self }
+        },
+        likesCount: 1,
+        createdAt: 1
+      }
+    : {};
+
+  return this.find(query, projection)
+    .sort({ _id: -1 }) // _id 역순
+    .limit(20) // 20개로 제한
+    .exec();
+};
+
 module.exports = mongoose.model("Post", Post);
